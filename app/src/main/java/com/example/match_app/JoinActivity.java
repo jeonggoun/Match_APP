1118ac478b1cdab7db69d6abc03872ed7192f5d2
DESKTOP-8AM7TK8\jeon <4evertop.jeon@gmail.com>
2021-05-23 18:50:11 +0900

    all xml modified, login activity, join activity updated

--- app/src/main/java/com/example/match_app/JoinActivity.java
+++ app/src/main/java/com/example/match_app/JoinActivity.java
@@ -1,14 +1,73 @@
 package com.example.match_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.match_app.asynctask.member.Login;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class JoinActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;         // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef;     // 실시간 데이터베이스
+    private EditText mEtEmail, mEtPwd;          // 회원가입 필드
+    private Button mBtnRegiseter;               // 회원가입 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pwd);
+        mBtnRegiseter = findViewById(R.id.btn_register);
+
+        mBtnRegiseter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 회원가입 처리 시작
+                String strEmail = mEtEmail.getText().toString().trim();
+                String strPwd = mEtPwd.getText().toString().trim();
+
+                // Firebase Auth 진행
+                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(JoinActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                            UserAccount account = new UserAccount();
+                            account.setIdToken(firebaseUser.getUid());      // 토큰정보 고유값
+                            account.setEmailId(firebaseUser.getEmail());
+                            account.setPassword(strPwd);
+
+                            // setValue : database에 insert(삽입) 행위
+                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                            Toast.makeText(JoinActivity.this, "회원가입을 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(JoinActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(JoinActivity.this, "회원가입을 실패하셨습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
