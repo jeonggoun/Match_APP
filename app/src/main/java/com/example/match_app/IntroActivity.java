1e8fe0e451502c0bb3288d599d3dfaecdeb5fea8
DESKTOP-8AM7TK8\jeon <4evertop.jeon@gmail.com>
2021-05-24 22:51:13 +0900

    intro, login01, login02, main 연동 수정필요함 문자인증 실행 완료

--- app/src/main/java/com/example/match_app/IntroActivity.java
+++ app/src/main/java/com/example/match_app/IntroActivity.java
@@ -5,18 +5,36 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class IntroActivity extends AppCompatActivity {
+    private static final String TAG = "IntroActivity MAIN : ";
+
+    private Button button;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-    }
 
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
+    }
 
     private void checkDangerousPermissions() {
         String[] permissions = {
@@ -62,6 +80,18 @@ public class IntroActivity extends AppCompatActivity {
                     Toast.makeText(this, permissions[i] + " 권한이 승인되지 않음.", Toast.LENGTH_LONG).show();
                 }
             }
+        }
+    }
+
+    // 현재 사용자 계정정보가 있는가? if =null 이면 로그인 액티비티로 가시오
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.d(TAG, "onStart: currentUser : "+currentUser);
+        if (currentUser == null) {
+            startActivity(new Intent(IntroActivity.this, Login02Activity.class));
+            finish();
         }
     }
 }
--- app/src/main/java/com/example/match_app/MainActivity.java
+++ app/src/main/java/com/example/match_app/MainActivity.java
@@ -2,18 +2,47 @@ package com.example.match_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.example.match_app.fragment.ChatListFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-//홈화면
-     ChatListFragment chatListFragment;
+
+    private static final String TAG = "IntroActivity MAIN : ";
+    private Button button;
+    private FirebaseAuth mAuth;
+    ChatListFragment chatListFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        chatListFragment = (ChatListFragment) getSupportFragmentManager().findFragmentById(R.id.contain);
+        setContentView(R.layout.activity_intro);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
+    }
+
+    // 현재 사용자 계정정보가 있는가? if =null 이면 로그인 액티비티로 가시오
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.d(TAG, "onStart: currentUser : "+currentUser);
+        if (currentUser == null) {
+            startActivity(new Intent(MainActivity.this, Login02Activity.class));
+            finish();
+        }
     }
 }
