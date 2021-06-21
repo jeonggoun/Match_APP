fcb3726af10cc1c00222eb42bb77aacad94810c5
drjeon92 <4evertop.jeon@gmail.com>
2021-06-21 11:07:51 +0900

    짜투리

--- app/src/main/java/com/example/match_app/fragment/EtcFragment.java
+++ app/src/main/java/com/example/match_app/fragment/EtcFragment.java
@@ -1,90 +1,70 @@
 package com.example.match_app.fragment;
 
-import android.content.Context;
+import android.location.Address;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-
-import com.example.match_app.MainActivity;
 import com.example.match_app.R;
-import com.example.match_app.adapter.IconViewer;
-import com.example.match_app.dto.IconDTO;
+import com.example.match_app.dto.MemberDTO;
+import com.example.match_app.dto.MetaDTO;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
+import java.lang.reflect.Member;
+
+import static com.example.match_app.MainActivity.user;
 
 public class EtcFragment extends Fragment {
+    MemberDTO dto;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); // 로그인한 유저의 정보 가져오기
+    String uid = user != null ? user.getUid() : null; // 로그인한 유저의 고유 uid 가져오기
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference;
 
-    GridView gv;
-    Context context;
-    IconAdapter iconAdapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("matchapp/UserAccount/"+uid);
         ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_etc, container, false);
         TextView tv_nick = viewGroup.findViewById(R.id.tv_nick);
         TextView tv_local = viewGroup.findViewById(R.id.tv_local);
-        Button btn_logout = viewGroup.findViewById(R.id.btn_logout);
         Button btn_profile = viewGroup.findViewById(R.id.btn_profile);
-        gv = viewGroup.findViewById(R.id.gridView);
 
-        iconAdapter = new IconAdapter();
-        iconAdapter.addItem(new IconDTO("모임목록", R.drawable.etcf1));
-        iconAdapter.addItem(new IconDTO("동네인증", R.drawable.etcf2));
-        iconAdapter.addItem(new IconDTO("선호목록", R.drawable.etcf3));
-        gv.setAdapter(iconAdapter);
-        return  viewGroup;
-    }
-
-    private class IconAdapter extends BaseAdapter {
-        private LayoutInflater inflater;
-        ArrayList<IconDTO> items = new ArrayList<IconDTO>();
-
-
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return items.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View convertView, ViewGroup parent) {
-            context = parent.getContext();
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.icon_item, parent, false);
+        // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                MemberDTO dto = dataSnapshot.getValue(MemberDTO.class);
             }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {            }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {            }
+        });
 
-            ImageView imageView = convertView.findViewById(R.id.iv_icon);
-            TextView textView = convertView.findViewById(R.id.tv_icon);
+        tv_nick.setText(dto.getNickName());
+        tv_local.setText(dto.getAddress());
 
-            IconDTO dto = items.get(i);
-            imageView.setImageResource(dto.getIconImage());
-            textView.setText(dto.getIconId());
-
-            return convertView;
-        }
-
-        public void addItem(IconDTO dto) {
-            items.add(dto);
-        }
+        return  viewGroup;
     }
 }
---
+++ app/src/main/java/com/example/match_app/fragment/EtcFragment2.java
@@ -0,0 +1,89 @@
+package com.example.match_app.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.match_app.R;
+import com.example.match_app.dto.IconDTO;
+
+import java.util.ArrayList;
+
+public class EtcFragment2 extends Fragment {
+
+    ListView lv;
+    GridView gv;
+    Context context;
+    IconAdapter iconAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_etc, container, false);
+        TextView tv_nick = viewGroup.findViewById(R.id.tv_nick);
+        TextView tv_local = viewGroup.findViewById(R.id.tv_local);
+        Button btn_profile = viewGroup.findViewById(R.id.btn_profile);
+/*        gv = viewGroup.findViewById(R.id.gridView);
+        lv = viewGroup.findViewById(R.id.listView);*/
+
+        iconAdapter = new IconAdapter();
+        iconAdapter.addItem(new IconDTO("모임목록", R.drawable.etcf1));
+        iconAdapter.addItem(new IconDTO("동네인증", R.drawable.etcf2));
+        iconAdapter.addItem(new IconDTO("선호목록", R.drawable.etcf3));
+        gv.setAdapter(iconAdapter);
+        return  viewGroup;
+    }
+
+    private class IconAdapter extends BaseAdapter {
+        private LayoutInflater inflater;
+        ArrayList<IconDTO> items = new ArrayList<IconDTO>();
+
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+            context = parent.getContext();
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.icon_item, parent, false);
+            }
+
+            ImageView imageView = convertView.findViewById(R.id.iv_icon);
+            TextView textView = convertView.findViewById(R.id.tv_icon);
+
+            IconDTO dto = items.get(i);
+            imageView.setImageResource(dto.getIconImage());
+            textView.setText(dto.getIconId());
+
+            return convertView;
+        }
+
+        public void addItem(IconDTO dto) {
+            items.add(dto);
+        }
+    }
+}
